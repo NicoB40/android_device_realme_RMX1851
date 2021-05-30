191fe2232afc09c1b1d38a7f196c9d4c0afbbda1
NicoB40 <belleau.nicolas@hotmail.fr>
2021-05-30 16:28:45 +0200

    Update update-sha1sums.py

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
+#!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,64 +15,25 @@
 # limitations under the License.
 #
 
-from hashlib import sha1
-import sys
+filename="proprietary-files.txt"
+filelines="$(cat $filename)"
+vendor="../../../vendor/realme/RMX1851/proprietary"
 
-device='RMX1851'
-vendor='realme'
+for line in $filelines ; do
+    if [[ $line == *"|"* ]]; then
 
-lines = [ line for line in open('proprietary-files.txt', 'r') ]
-vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
-needSHA1 = False
+        # Get the name of the blob and remove '-'
+        # when it is the first character
+        blob=$(echo $line | cut -f1 -d'|' | sed 's/^-//g')
 
-def cleanup():
-  for index, line in enumerate(lines):
-    # Remove '\n' character
-    line = line[:-1]
+        # Get the old sha1sum
+        oldSHA1=$(echo $line | cut -f2 -d'|')
 
-    # Skip empty or commented lines
-    if len(line) == 0 or line[0] == '#':
-      continue
+        # Get the new sha1sum from vendor path
+        newSHA1=$(sha1sum $vendor/$blob | awk '{print $1}')
 
-    # Drop SHA1 hash, if existing
-    if '|' in line:
-      line = line.split('|')[0]
-      lines[index] = '%s\n' % (line)
+        # Replace oldSHA1 with newSHA1
+        sed -i "s/$oldSHA1/$newSHA1/g" "$filename"
 
-def update():
-  for index, line in enumerate(lines):
-    # Remove '\n' character
-    line = line[:-1]
-
-    # Skip empty lines
-    if len(line) == 0:
-      continue
-
-    # Check if we need to set SHA1 hash for the next files
-    if line[0] == '#':
-      needSHA1 = (' - from' in line)
-      continue
-
-    if needSHA1:
-      # Remove existing SHA1 hash
-      line = line.split('|')[0]
-      filePath = line.split(':')[1] if len(line.split(':')) == 2 else line
-
-      if filePath[0] == '-':
-        file = open('%s/%s' % (vendorPath, filePath[1:]), 'rb').read()
-      else:
-        file = open('%s/%s' % (vendorPath, filePath), 'rb').read()
-
-      hash = sha1(file).hexdigest()
-      lines[index] = '%s|%s\n' % (line, hash)
-
-if len(sys.argv) == 2 and sys.argv[1] == '-c':
-  cleanup()
-else:
-  update()
-
-with open('proprietary-files.txt', 'w') as file:
-  for line in lines:
-    file.write(line)
-
-  file.close()
+    fi
+done
